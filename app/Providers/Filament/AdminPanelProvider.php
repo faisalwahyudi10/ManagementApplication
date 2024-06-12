15cc9f1daced37b29560709d0c0a9e77daec1a3e
faisalwahyudi10 <faisal.piffent.s@gmail.com>
2024-06-12 20:18:10 +0800

    feat: Add custom navigation to the Filament admin panel

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,9 +2,14 @@
 
 namespace App\Providers\Filament;
 
+use App\Models\Enums\MenuType;
+use App\Models\Menu;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Filament\Navigation\NavigationBuilder;
+use Filament\Navigation\NavigationGroup;
+use Filament\Navigation\NavigationItem;
 use Filament\Pages;
 use Filament\Panel;
 use Filament\PanelProvider;
@@ -16,6 +21,7 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\AuthenticateSession;
 use Illuminate\Session\Middleware\StartSession;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
 
 class AdminPanelProvider extends PanelProvider
@@ -46,10 +52,7 @@ class AdminPanelProvider extends PanelProvider
             ->resources([
                 config('filament-logger.activity_resource')
             ])
-            ->navigationGroups([
-                'Management Users',
-                'Developer Tools',
-            ])
+            ->navigation(static::getNavigations())          
             ->middleware([
                 EncryptCookies::class,
                 AddQueuedCookiesToResponse::class,
@@ -64,5 +67,70 @@ class AdminPanelProvider extends PanelProvider
             ->authMiddleware([
                 Authenticate::class,
             ]);
+    }
+
+    protected static function getNavigations()
+    {
+        return function (NavigationBuilder $builder): NavigationBuilder {
+
+            $user = Auth::user();
+            $menuGroups = Menu::query()
+                ->with(['children' => function ($query) {
+                    $query->orderBy('order');
+                }])
+                ->type(MenuType::Group)
+                ->whereIsShow(true)
+                ->whereParentId(null)
+                ->orderBy('order');
+
+            $menuItems = Menu::query()
+                ->whereParentId(null)
+                ->whereIsShow(true)
+                ->type([MenuType::Custom, MenuType::Resources])
+                ->orderBy('order');
+
+            $listGroups = [];
+            $listItems = [];
+
+            foreach ($menuGroups->get() as $menu) {
+                $listGroups[] = NavigationGroup::make()
+                    ->label($menu->name)
+                    ->items(static::getNavigationGroupItems($menu->children))
+                    ->when($menu->icon, fn ($group) => $group->icon($menu->icon));
+            }
+
+            foreach ($menuItems->get() as $item) {
+                $listItems[] = NavigationItem::make()
+                    ->label($item->name)
+                    ->icon($item->icon)
+                    ->url(route($item->route));
+            }
+
+            return $builder
+                ->groups($listGroups)
+                ->items($listItems);
+        };
+    }
+
+    protected static function getNavigationGroupItems($menu): array
+    {
+        $listItem = [];
+
+        foreach ($menu as $child) {
+            $instance = $child->instance;
+            if ($child->type == MenuType::Resources)
+            {
+                $listItem = array_merge($listItem, $instance::getNavigationItems());
+                
+            } else {
+                $listItem[] = NavigationItem::make()
+                    ->label($child->name)
+                    ->icon($child->icon)
+                    ->sort($child->order)
+                    ->url(route($child->route));
+            }
+        }
+
+        return $listItem;
     }
 }
